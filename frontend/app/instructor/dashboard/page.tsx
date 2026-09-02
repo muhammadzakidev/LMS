173346ff4c00ed26719d5e866b53d586e76eee1a
mzakiDev-eng <muhamadzakidev@gmail.com>
2026-09-02 15:21:22 -0700

    Add course feature and also fix dropdown trigger

--- frontend/app/instructor/dashboard/page.tsx
+++ frontend/app/instructor/dashboard/page.tsx
@@ -1,5 +1,7 @@
 import {redirect} from "next/navigation"
 import { getAuthSession } from "@/lib/auth";
+import { BookOpen ,FileText , CircleCheck } from "lucide-react";
+import {Card , CardContent , CardHeader, CardTitle } from "@/components/ui/card"
 export default async function InstructorDashboard()
 {
   const session = await getAuthSession();
@@ -12,14 +14,62 @@ export default async function InstructorDashboard()
     redirect('/403');
   }
    return (
-    <>
-     <main className="p-4">
-        <h1 className="text-6xl font-bold text-center">Welcome to Instructor Dashboard</h1>
-        <h3 className="mt-6 text-3xl text-center">
-            Welcome, {session.user.name}! You are logged in as a {session.user.role}.
-        </h3>
+    <div className="space-y-6">
+      <div>
+        <h1 className="text-3xl font-bold">
+          Dashboard
+        </h1>
+        <p className="text-muted-foreground">
+          Mange your courses or content.
+        </p>
+      </div>
+      <div className="grid gap-4 md:grid-cols-3">
+        <Card>
+          <CardHeader className="flex  flex-row items-center justify-between">
+            <CardTitle className="text-sm font-medium">
+              Total Courses
+            </CardTitle>
+            <BookOpen className="h-4 w-4 text-muted-foreground"/>
 
-     </main>
-    </>
+          </CardHeader>
+
+          <CardContent>
+            <div className="text-2xl font-bold">
+              0
+            </div>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between">
+            <CardTitle className="text-sm font-medium">
+              Published
+            </CardTitle>
+            <CircleCheck className="h-4 w-4 text-muted-foreground"/>
+          </CardHeader>
+            <CardContent>
+            <div className="text-2xl font-bold">
+              0
+            </div>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-center">
+            <CardTitle className="text-sm font-medium">
+              Draft
+            </CardTitle>
+            <FileText className="h-4 w-4 text-muted-foreground"/>
+          </CardHeader>
+            <CardContent>
+            <div className="text-2xl font-bold">
+              0
+            </div>
+          </CardContent>
+        </Card>
+
+      </div>
+     
+    </div>
    )
 }
--- frontend/components/instructor/instructor-header.tsx
+++ frontend/components/instructor/instructor-header.tsx
@@ -5,3 +5,60 @@ import {Avatar , AvatarFallback} from '../ui/avatar'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem,
     DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger
  } from "../ui/dropdown-menu";
+
+
+ interface InstructorHeaderType{
+    name: string ,
+    email: string
+ }
+
+ export default function InstructorHeader({
+    name, email,
+ }: InstructorHeaderType) {
+    return (
+        <header className="flex h-16 items-center justify-center border-b px-6">
+            <div className="flex items-center gap-3">
+                <SidebarTrigger/>
+                <h2 className="font-semibold">
+                    Course Management System
+                </h2>
+
+            </div>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <button className="flex items-center gap-3 rounded-md p-2 hover:bg-muted">
+                        <div className="hidden text-right sm:block">
+                            <p className="text-sm font-medium">{name}</p>
+                            <p className="text-xs text-shadow-muted-foreground">{email}</p>
+                        </div>
+                        <Avatar>
+                            <AvatarFallback>
+                                {name.charAt(0).toUpperCase()}
+                            </AvatarFallback>
+                        </Avatar>
+
+                    </button>
+
+                    
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="w-56">
+                    <DropdownMenuLabel>
+                    My Account
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator/>
+                 <DropdownMenuItem>
+                    <User/>
+                    Profile
+                 </DropdownMenuItem>
+                 <DropdownMenuItem>
+                    <LogOut/>
+                    Logout
+                 </DropdownMenuItem>
+                  
+                </DropdownMenuContent>
+                
+            </DropdownMenu>
+
+        </header>
+    );
+ }
