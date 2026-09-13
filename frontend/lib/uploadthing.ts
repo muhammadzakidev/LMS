869adfaa929feb954663f8dff457ac222a29be6b
mzakiDev-eng <muhamadzakidev@gmail.com>
2026-09-12 20:45:35 -0700

    fix uploadthing frontend config

--- frontend/lib/uploadthing.ts
+++ frontend/lib/uploadthing.ts
@@ -1,7 +1,5 @@
 // cspell:disable-next-line
 import { generateUploadButton } from "@uploadthing/react";
-import type { OurFileRouter } from "../app/api/uploadthing/core";
-
-export const UploadButton = generateUploadButton<OurFileRouter>({
-  url: "http://localhost:5000/api/uploadthing",
+export const UploadButton = generateUploadButton({
+  url: `${process.env.NEXT_PUBLIC_API_URL}/api/uploadthing`,
 });
